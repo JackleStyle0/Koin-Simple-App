d48739e5b52d2bf23a8ab92de31005abae189bfa
Wacharat <wacharat.th@gmail.com>
2019-12-07 14:26:00 +0700

    inject to activity

--- app/src/main/java/com/jackle/koinsimple/MainActivity.kt
+++ app/src/main/java/com/jackle/koinsimple/MainActivity.kt
@@ -2,11 +2,17 @@ package com.jackle.koinsimple
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+
+    val firstPresenter: MySimplePresenter by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("Say", "${firstPresenter.sayHello()}")
     }
 }
