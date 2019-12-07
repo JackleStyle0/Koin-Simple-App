90cb252bfdfaf324ab21f95e9a5aeaae3d3d922f
Wacharat <wacharat.th@gmail.com>
2019-12-07 15:15:19 +0700

    setup viewmodel and view

--- app/src/main/java/com/jackle/koinsimple/MainActivity.kt
+++ app/src/main/java/com/jackle/koinsimple/MainActivity.kt
@@ -3,16 +3,32 @@ package com.jackle.koinsimple
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
-    val firstPresenter: MySimplePresenter by inject()
+    private val userViewModel by viewModel<UserViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("Say", "${firstPresenter.sayHello()}")
+        userViewModel.data.observe(this, Observer {
+            // Todo: Populate the recyclerView here
+            it.forEach { githubUser ->
+                Toast.makeText(baseContext, githubUser.login, Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        userViewModel.loadingState.observe(this, Observer {
+            when (it.status) {
+                LoadingState.Status.FAILED -> Toast.makeText(baseContext, it.msg, Toast.LENGTH_SHORT).show()
+                LoadingState.Status.RUNNING -> Toast.makeText(baseContext, "Loading", Toast.LENGTH_SHORT).show()
+                LoadingState.Status.SUCCESS -> Toast.makeText(baseContext, "Success", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
